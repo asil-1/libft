4705115c6a83b8633e961b73e39fa6ac0fc25511
Lisa Depenne <ldepenne@3A3.42angouleme.fr>
2025-10-23 17:19:58 +0200

    add atoi

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldepenne <ldepenne@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 16:58:34 by ldepenne          #+#    #+#             */
+/*   Updated: 2025/10/23 17:18:46 by ldepenne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_atoi(const char *nptr)
+{
+	int	s;
+	int	nb;
+
+	s = 1;
+	nb = 0;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			s *= -1;
+		nptr++;
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		nb = nb * 10 + (*nptr - '0');
+		nptr++;
+	}
+	return (nb * s);
+}
+
+// int	main(void)
+// {
+// 	char	*nptr = "   -46371";
+
+// 	printf("%d\n", ft_atoi(nptr));
+// 	printf("%d\n", atoi(nptr));
+// }
--- libft.h
+++ libft.h
@@ -6,13 +6,13 @@
 /*   By: ldepenne <ldepenne@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 16:29:19 by ldepenne          #+#    #+#             */
-/*   Updated: 2025/10/23 14:56:43 by ldepenne         ###   ########.fr       */
+/*   Updated: 2025/10/23 17:18:31 by ldepenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-#include <stdlib.h>
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -34,5 +34,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
 
 #endif
