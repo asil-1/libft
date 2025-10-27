7e6f9db2d5b19b167a4363ae6f22371ae5744bbd
Lisa Depenne <ldepenne@3B4.42angouleme.fr>
2025-10-27 11:34:19 +0100

    bwjkefgwke

--- ft_split.c
+++ ft_split.c
@@ -6,22 +6,18 @@
 /*   By: ldepenne <ldepenne@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 18:28:27 by ldepenne          #+#    #+#             */
-/*   Updated: 2025/10/26 16:51:43 by ldepenne         ###   ########.fr       */
+/*   Updated: 2025/10/27 11:32:59 by ldepenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	uhgeuigrh
-// }
-
-int	ft_count_word(char *s, char sep)
+// attention -> proteger avant, si !s segfault
+size_t	ft_count_word(char *s, char sep)
 {
-	int	i;
-	int	word;
+	size_t	i;
+	size_t	word;
 
 	i = 0;
 	word = 0;
@@ -38,10 +34,56 @@ int	ft_count_word(char *s, char sep)
 	return (word);
 }
 
+char	**ft_split(char const *s, char c)
+{
+	char	**new;
+	size_t	n;
+	size_t	i;
+	size_t	start;
+	size_t	len;
+	size_t	nb_words;
+
+	nb_words = ft_count_word((char *) s, c);
+	new = malloc(sizeof(char *) * (nb_words + 1));
+	if (!new)
+		return (NULL);
+	n = 0;
+	while (n <= nb_words)
+	{
+		i = 0;
+		start = 0;
+		len = 0;
+		while (s[i] == c)
+		{
+			start++;
+			i++;
+		}
+		while (s[i])
+		{
+			if (s[i] == c)
+				break;
+			len++;
+			i++;
+		}
+		new[n] = ft_substr(s, start, len);
+		s = &s[i];
+		n++;
+		printf("%zu\n", n);
+	}
+	return (new);
+}
+
 int	main(void)
 {
-	char	s[] = "..les.feuilles.sont..oranges.";
+	char	s[] = "..les.feuilles.sont..oranges..";
 	char	sep = '.';
+	size_t	i = 0;
+	char	**print;
 
-	printf("%d\n", ft_count_word(s, sep));
+	print = ft_split(s, sep);
+	while (i <= ft_count_word(s, sep))
+	{
+		printf("%s\n", print[i]);
+		i++;
+	}
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ldepenne <ldepenne@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 14:36:31 by ldepenne          #+#    #+#             */
-/*   Updated: 2025/10/25 18:15:46 by ldepenne         ###   ########.fr       */
+/*   Updated: 2025/10/27 10:10:28 by ldepenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 // int	main(void)
 // {
-// 	char	s1[] = "kayakkayak";
+// 	char	s1[] = "kayakraccoonkayak";
 // 	char	set[] = "yka";
 // 	char	*trim;
 
