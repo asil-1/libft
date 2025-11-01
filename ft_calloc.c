576e87a297c35704404b148f319256bbdcda280c
Lisa Depenne <ldepenne@2G2.42angouleme.fr>
2025-11-01 15:27:46 +0100

    fix return (malloc(0));

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ldepenne <ldepenne@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 17:22:49 by ldepenne          #+#    #+#             */
-/*   Updated: 2025/10/30 19:20:12 by ldepenne         ###   ########.fr       */
+/*   Updated: 2025/11/01 15:24:47 by ldepenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	*result;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return (malloc(0));
 	if (nmemb > (size_t) - 1 / size)
 		return (NULL);
 	result = malloc(nmemb * size);
